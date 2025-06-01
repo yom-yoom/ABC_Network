8eb304e0870b13708a9c247d08ec2b12b4e9532a
Max Goloviznin <roxet59rus@mail.ru>
2025-06-01 23:33:27 +0500

    run building project

---
+++ add_submodules.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+git submodule add -b Yom-stream9 https://github.com/yom-yoom/user_service user_service
+git submodule add -b Yom-stream9 https://github.com/yom-yoom/project_service project_service
+git submodule add -b Yom-master-stream9 https://github.com/yom-yoom/post_service post_service
+git submodule add -b Yom-master-stream9 https://github.com/yom-yoom/url_shortener_service url_shortener_service
+git submodule add -b Yom-master-stream9 https://github.com/yom-yoom/hashtag_service hashtag_service
+git submodule add -b Yom-stream9 https://github.com/yom-yoom/account_service account_service
+git submodule add -b Yom-stream9 https://github.com/yom-yoom/payment_service payment_service
+git submodule add -b Yom-stream9 https://github.com/yom-yoom/notification_service notification_service
+git submodule add -b Yom-stream9 https://github.com/yom-yoom/achievement_service achievement_service
+git submodule add -b Yom https://github.com/yom-yoom/security_service security_service
+git submodule add -b Yom-stream9 https://github.com/yom-yoom/analytics_service analytics_service
+git submodule add -b Yom https://github.com/yom-yoom/AI_service AI_service
+git submodule add -b Yom-stream9 https://github.com/yom-yoom/infra_service infra_service
